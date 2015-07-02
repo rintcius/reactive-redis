069e65b3c397ffe2d65bc3960c1ed00707623438
rintcius <rintcius@gmail.com>
2015-07-02 15:32:35 +0200

    use java driver

--- driver-rediscala/src/test/java/net/cakesolutions/reactiveredis/driver/japi/RediscalaCommandsTest.java
+++ driver-rediscala/src/test/java/net/cakesolutions/reactiveredis/driver/japi/RediscalaCommandsTest.java
@@ -2,13 +2,13 @@ package net.cakesolutions.reactiveredis.driver.japi;
 
 import akka.actor.ActorSystem;
 import net.cakesolutions.reactiveredis.driver.api.RedisDriver;
-import net.cakesolutions.reactiveredis.driver.rediscala.RediscalaDriver$;
+import net.cakesolutions.reactiveredis.jdriver.rediscala.RediscalaJavaDriver;
 import scala.concurrent.ExecutionContext;
 
 public class RediscalaCommandsTest extends CommandsTest {
 
     @Override
     public RedisDriver redisDriver() {
-        return RediscalaDriver$.MODULE$.rediscalaDriver("localhost", 6379, ActorSystem.apply(), ExecutionContext.Implicits$.MODULE$.global());
+        return new RediscalaJavaDriver("localhost", 6379, ActorSystem.apply(), ExecutionContext.Implicits$.MODULE$.global());
     }
 }
